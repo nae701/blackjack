830ce340e2dac22c548e8b0431e92d0fc94e47da
nae701 <43119228+nae701@users.noreply.github.com>
2019-01-14 12:21:26 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -42,19 +42,19 @@ def num_ace(hand):
 
 def dealer_game(hand, hit, score_hit, total, number_ace):
     """Executes the recursive game logic for the dealer and returns their score"""
-
     score_initial = get_value(hand[0], total) + get_value(hand[1], total)
     score = score_initial + score_hit
     print("DEALER:" + str(hand), '\n')
     print("Dealer's score: " + str(score), '\n')
 
-    #
+    # Dealer must hit if below 17
     if score < 17:
         hand.append(deck.pop())
         hit += 1
         score_hit = score_hit + get_value(hand[1 + hit], score)
         return dealer_game(hand, hit, score_hit, score, number_ace)
 
+    # Checks if dealer has a soft hand and can continue
     elif score > 21:
         if number_ace > 0:
             if num_ace([hand[1 + hit]]) > 0:
@@ -63,29 +63,33 @@ def dealer_game(hand, hit, score_hit, total, number_ace):
 
         print("DEALER BUSTS.\n")
         return 1
+
     elif score == 21:
         return score
+
     else:
         return score
 
 
 def player_game(hand, hit, score_hit, total, number_ace):
     """Executes the recursive game logic for the player and returns their score"""
-    print("PLAYER:" + str(hand), '\n')
     score_initial = get_value(hand[0], total) + get_value(hand[1], total)
     score = score_initial + score_hit
+    print("PLAYER:" + str(hand), '\n')
     print("Player's score: " + str(score), '\n')
     player_choice = input("Would you like to 'HIT' or 'STAY': \n")
 
     if player_choice.lower() == "stay":
         return score
 
+    # Adds a card to player's hand and determines new score
     elif player_choice.lower() == 'hit':
         hand.append(deck.pop())
         hit += 1
         score_hit = score_hit + get_value(hand[1 + hit], score)
         score = score_initial + score_hit
 
+        # Check if player busts or can continue with ace as 1 now
         if score > 21:
             if number_ace > 0:
                 if num_ace([hand[1 + hit]]) > 0:
@@ -96,9 +100,12 @@ def player_game(hand, hit, score_hit, total, number_ace):
             print("Player's score: " + str(score), '\n')
             print("YOU BUST.\n")
             return 0
+
         elif score == 21:
             return score
+
         return player_game(hand, hit, score_hit, score_hit, number_ace)
+
     else:
         print("Choose to either 'HIT' or 'STAY'.\n")
         return player_game(hand, hit, score_hit, score, number_ace)
@@ -120,15 +127,18 @@ if __name__ == '__main__':
     keep_playing = True
 
     while keep_playing:
+
         # get a deck of cards, and randomly shuffle it
         deck = get_deck()
         random.shuffle(deck)
 
         wager = wager_amount(money)
 
+        # Checks if player wants to quit
         if wager == 0:
             sys.exit()
 
+        # Deals each hand, removing the cards from the deck
         dealer_hand = [deck.pop(), deck.pop()]
         player_hand = [deck.pop(), deck.pop()]
 
@@ -140,6 +150,7 @@ if __name__ == '__main__':
         player_score_initial = get_value(player_hand[0], 0) + get_value(player_hand[1], 0)
         dealer_score_initial = get_value(dealer_hand[0], 0) + get_value(dealer_hand[1], 0)
 
+        # Prints out game's outcome and money won/lost
         if player_score > dealer_score:
             if player_score_initial == 21:
                 money = money + 1.5 * wager
